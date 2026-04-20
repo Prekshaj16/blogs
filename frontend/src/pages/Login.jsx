51dbd5b03c1723985c8728e3d37803f63f9cc37e
Prekshaj16 <prekshajainsurat@gmail.com>
2026-04-20 22:59:42 +0530

    fixed login error

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
             headers: {
                 "Content-Type": "application/json",
             },
-            credentials: "include", // 🔥 VERY IMPORTANT (for cookies)
+            credentials: "include", 
             body: JSON.stringify({ email, password }),
         });
 
